00af99b9017921a5e3a6d83b9460b460618e4719
huanniangstudio <891863891@qq.com>
2023-04-04 15:14:14 +0800

    feat: add createSendMultiOrds

--- src/index.ts
+++ src/index.ts
@@ -189,3 +189,95 @@ export async function createSendOrd({
 
   return psbt;
 }
+
+export async function createSendMultiOrds({
+  utxos,
+  toAddress,
+  toOrdIds,
+  wallet,
+  network,
+  changeAddress,
+  pubkey,
+  feeRate,
+}: {
+  utxos: UnspentOutput[];
+  toAddress: string;
+  toOrdIds: string[];
+  wallet: any;
+  network: any;
+  changeAddress: string;
+  pubkey: string;
+  feeRate?: number;
+}) {
+  const tx = new OrdTransaction(wallet, network, pubkey, feeRate);
+  tx.setChangeAddress(changeAddress);
+
+  const nonOrdUtxos: UnspentOutput[] = [];
+  const ordUtxos: UnspentOutput[] = [];
+  utxos.forEach((v) => {
+    if (v.ords.length > 0) {
+      ordUtxos.push(v);
+    } else {
+      nonOrdUtxos.push(v);
+    }
+  });
+
+  // find NFT
+  let foundedCount = 0;
+
+  for (let i = 0; i < ordUtxos.length; i++) {
+    const ordUtxo = ordUtxos[i];
+    if (ordUtxo.ords.find((v) => toOrdIds.includes(v.id))) {
+      if (ordUtxo.ords.length > 1) {
+        throw new Error(
+          "Multiple inscriptions in one UTXO! Please split them first."
+        );
+      }
+      tx.addInput(ordUtxo);
+      tx.addOutput(toAddress, ordUtxo.satoshis);
+      foundedCount++;
+    }
+  }
+
+  if (foundedCount != toOrdIds.length) {
+    throw new Error("inscription not found.");
+  }
+
+  // Do not format NFT
+  // tx.outputs[0].value = outputValue;
+
+  nonOrdUtxos.forEach((v) => {
+    tx.addInput(v);
+  });
+
+  const unspent = tx.getUnspent();
+  if (unspent == 0) {
+    throw new Error("Balance not enough to pay network fee.");
+  }
+
+  // add dummy output
+  tx.addChangeOutput(1);
+
+  const networkFee = await tx.calNetworkFee();
+  if (unspent < networkFee) {
+    throw new Error(
+      `Balance not enough. Need ${satoshisToAmount(
+        networkFee
+      )} BTC as network fee, but only ${satoshisToAmount(unspent)} BTC.`
+    );
+  }
+
+  const leftAmount = unspent - networkFee;
+  if (leftAmount >= UTXO_DUST) {
+    // change dummy output to true output
+    tx.getChangeOutput().value = leftAmount;
+  } else {
+    // remove dummy output
+    tx.removeChangeOutput();
+  }
+
+  const psbt = await tx.createSignedPsbt();
+  // tx.dumpTx(psbt);
+
+  return psbt;
+}
